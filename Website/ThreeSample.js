ca4fb7e7b2584ce2016ec442a427f7e8818fa587
xVennce <145119672+xVennce@users.noreply.github.com>
2024-10-25 16:35:00 +0100

    Updated the ThreeSample.js
    Added orbit controls to the game page.

--- Website/ThreeSample.js
+++ Website/ThreeSample.js
@@ -1,41 +1,107 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
+
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 /*Creating box geometry*/
-const geometry1 = new THREE.BoxGeometry( 1, 1, 1 );
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 /*Creating basic material*/
-const material1 = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 /*Creating mesh*/
-const cube = new THREE.Mesh( geometry1, material1 );
+const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-const geometry2 = new THREE.TorusKnotGeometry( 10, 3, 100, 16 ); 
-const material2 = new THREE.MeshBasicMaterial( { color: 0xffff00 } ); 
-const torusKnot = new THREE.Mesh( geometry2, material2 ); 
-scene.add( torusKnot );
-
-
+const geometrySB = new THREE.BoxGeometry( 100, 100, 100 );
+/*Creating basic material*/
+const materialSB = new THREE.MeshBasicMaterial( { color:  0x6930c3, side: THREE.DoubleSide} );
+/*Creating mesh*/
+const cubeSB = new THREE.Mesh( geometrySB, materialSB );
+scene.add( cube, cubeSB );
 cube.position.x = (0);
-torusKnot.position.x = (50);
 
-torusKnot.parent = cube;
+camera.position.z = 5;
 
-camera.position.z = 50;
-function animate() {
+let controls;
 
-    torusKnot.rotation.x +=0.05;
-    torusKnot.rotation.y +=0.05;
+const createControls = () =>{
+    controls = new OrbitControls(camera, renderer.domElement);
 
+};
+
+createControls();
+
+controls.enableDamping=true;
+controls.dampingFactor=0.05;
+
+controls.screenSpacePanning=false;
+
+
+
+/*Controls*/
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+let rightPressed = false;
+let leftPressed = false;
+let upPressed = false;
+let downPressed = false;
+
+function keyDownHandler(event){
+	if (event.keyCode === 68){
+		rightPressed = true;
+	}
+	else if (event.keyCode === 65){
+		leftPressed = true;
+	}
+	else if (event.keyCode === 83){
+		downPressed = true;
+	}
+	else if (event.keyCode === 87){
+		upPressed = true;
+	}
+}
+function keyUpHandler(event){
+	if (event.keyCode === 68){
+		rightPressed = false;
+	}
+	if (event.keyCode === 65){
+		leftPressed = false;
+	}
+	if (event.keyCode === 83){
+		downPressed = false;
+	}
+	if (event.keyCode === 87){
+		upPressed = false;
+	}
+}
+const speed = 0.01;
+/*Controls*/
+
+const animate=function() {
+	
+	if (rightPressed) {
+        cube.position.x += speed;
+    }
+    if (leftPressed) {
+        cube.position.x -= speed;
+    }
+    if (upPressed) {
+        cube.position.y += speed;
+    }
+    if (downPressed) {
+        cube.position.y -= speed;
+    }
+
+	console.log(renderer);
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
-
 	renderer.render( scene, camera );
-
 }
+
+renderer.setAnimationLoop(animate);
