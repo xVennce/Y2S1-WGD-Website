ef86c54950efc64565f3c12aa6356ec7673ef92a
xVennce <145119672+xVennce@users.noreply.github.com>
2024-11-01 14:39:28 +0000

    Added 3D model into scene and added add and remove function

--- Website/resources/js/ThreeSample.js
+++ Website/resources/js/ThreeSample.js
@@ -1,17 +1,18 @@
 import * as THREE from 'three';
-import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
-import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { upPressed, downPressed, leftPressed, rightPressed, deletePressed, insertPressed } from './InputCheck.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
+const loader = new GLTFLoader();
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 /*Creating box geometry*/
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const geometry = new THREE.BoxGeometry( .1, .1, .1 );
 /*Creating basic material*/
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 /*Creating mesh*/
@@ -23,82 +24,75 @@ const geometrySB = new THREE.BoxGeometry( 100, 100, 100 );
 const materialSB = new THREE.MeshBasicMaterial( { color:  0x6930c3, side: THREE.DoubleSide} );
 const cubeSB = new THREE.Mesh( geometrySB, materialSB );
 scene.add( cube, cubeSB );
+
+//Creating white directional light from top
+const directionalLight = new THREE.AmbientLight(0x404040);
+scene.add(directionalLight);
+
 //setting cube x pos
 cube.position.x = (0);
 
+
+//Adding 3D model to scene
+
+let mesh;
+
+loader.load(
+	'../resources/3DModels/helicopter.glb',
+	(gltf) => {
+		mesh = gltf.scene;
+		mesh.scale.set(0.3, 0.3, 0.3);
+		//adds GLTF to the scene
+		scene.add(mesh);
+	},
+	//called when loading is in progress
+	(xhr) => {
+		console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+	},
+	//called when loading has errors
+	(error) => {
+		console.log('Error has occured!' + error);
+	}
+);
+
+
+//Controls
 camera.position.z = 5;
 
 let controls;
-
 const createControls = () =>{
     controls = new OrbitControls(camera, renderer.domElement);
-
 };
-
 createControls();
-
 controls.enableDamping=true;
 controls.dampingFactor=0.05;
-
 controls.screenSpacePanning=false;
 
-
-
-/*Controls*/
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-
-let rightPressed = false;
-let leftPressed = false;
-let upPressed = false;
-let downPressed = false;
-
-function keyDownHandler(event){
-	if (event.keyCode === 68){
-		rightPressed = true;
-	}
-	else if (event.keyCode === 65){
-		leftPressed = true;
-	}
-	else if (event.keyCode === 83){
-		downPressed = true;
-	}
-	else if (event.keyCode === 87){
-		upPressed = true;
-	}
-}
-function keyUpHandler(event){
-	if (event.keyCode === 68){
-		rightPressed = false;
-	}
-	if (event.keyCode === 65){
-		leftPressed = false;
-	}
-	if (event.keyCode === 83){
-		downPressed = false;
-	}
-	if (event.keyCode === 87){
-		upPressed = false;
-	}
-}
 const speed = 0.01;
-/*Controls*/
 
+//applying movement
 const animate=function() {
 	
 	if (rightPressed) {
-        cube.position.x += speed;
+        mesh.position.x += speed;
     }
     if (leftPressed) {
-        cube.position.x -= speed;
+        mesh.position.x -= speed;
     }
     if (upPressed) {
-        cube.position.y += speed;
+        mesh.position.y += speed;
     }
     if (downPressed) {
-        cube.position.y -= speed;
+        mesh.position.y -= speed;
     }
-
+	if (deletePressed){
+		scene.remove(mesh);
+		geometry.dispose();
+		material.dispose();
+	}
+	if (insertPressed){
+		scene.add(mesh);
+	}
 	console.log(renderer);
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
