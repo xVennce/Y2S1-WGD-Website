9c93c8419f0360f167b30d401368a07a0ff5abc4
xVennce <145119672+xVennce@users.noreply.github.com>
2025-01-03 07:14:28 +0000

    Created an async function that logs the current user into the userdata.json

--- nodejsProject/myGame/app.js
+++ nodejsProject/myGame/app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const notifier = require('node-notifier');
-const fs = require('fs/promises');
-const path = require('path');
+const EXPRESS = require('express');
+const APP = EXPRESS();
+const PORT = 3000;
+const BODYPARSER = require('body-parser');
+const BCRYPT = require('bcrypt');
+const NOTIFIER = require('node-notifier');
+const FS = require('fs/promises');
+const PATH = require('path');
 
 var mysql = require('mysql');
 
@@ -17,12 +17,12 @@ var con = mysql.createConnection({
 });
 
 //setting up static folder
-app.use(express.static(__dirname + "/static"));
+APP.use(EXPRESS.static(__dirname + "/static"));
 //setting up secure folder
 
 //setting up body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+APP.use(BODYPARSER.json());
+APP.use(BODYPARSER.urlencoded({ extended: true }));
 
 con.connect(function(err) {
   if (err) throw err;
@@ -36,23 +36,23 @@ con.connect(function(err) {
 //this function should add the user to the database
 //on error, it sends a desktop notif
 function registerUser(username, password, res) {
-  const saltRounds = 10;
-  const score = 0;
-  bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
+  const SALTROUNDS = 10;
+  const SCORE = 0;
+  BCRYPT.hash(password, SALTROUNDS, (err, hashedPassword) => {
     if (err) {
       console.error('Error hashing the password:', err);
-      return notifier.notify({
+      return NOTIFIER.notify({
         title: 'Error',
         message: 'Error occured when trying to hash password. Try again.',
       });
     }
     //this query checks if the username has already been taken
-    const usernameCheck = 'SELECT * FROM userinfo WHERE username = ?';
-    con.query(usernameCheck, [username], function (err, result) {
+    const USERNAMECHECK = 'SELECT * FROM userinfo WHERE username = ?';
+    con.query(USERNAMECHECK, [username], function (err, result) {
       if (err) {
         userCheck = false;
         console.error('Error occured when checking for existing usernames.')
-        return notifier.notify({
+        return NOTIFIER.notify({
           title: 'Error',
           message: 'Error occured when checking for existing usernames. Try again.',
         });
@@ -60,18 +60,18 @@ function registerUser(username, password, res) {
       if (result.length > 0) {
         userCheck = false;
         console.log('Error: Username already exists');
-        return notifier.notify({
+        return NOTIFIER.notify({
           title: 'Error',
           message: 'Username already exists. Try again.',
         });
       }
       else{
       //this statement only runs if the username is unique
-        const query = 'INSERT INTO userinfo (Username, Password, Score) VALUES (?, ?, ?)';
-        con.query(query, [username, hashedPassword, score], (err, results) => {
+        const QUERY = 'INSERT INTO userinfo (Username, Password, Score) VALUES (?, ?, ?)';
+        con.query(QUERY, [username, hashedPassword, SCORE], (err, results) => {
           if (err) {
             console.error('Error inserting user:', err);
-            return notifier.notify({
+            return NOTIFIER.notify({
               title: 'Error',
               message: 'Error occured when trying to insert user into table. Try again.',
             });
@@ -87,33 +87,34 @@ function registerUser(username, password, res) {
 //this function should check for the login details
 //on error, it sends a desktop notif
 function loginUser(username, password, res) {
-  const query = 'SELECT * FROM userinfo WHERE Username = ?';
-  con.query(query, [username], (err, results) => {
+  const QUERY = 'SELECT * FROM userinfo WHERE Username = ?';
+  con.query(QUERY, [username], (err, results) => {
     if (err) {
       //this occurs when there is an error when trying to fetch usernames
       console.error('Error fetching user:', err);
-      return notifier.notify({
+      return NOTIFIER.notify({
         title: 'Error',
         message: 'Error fetching usernames. Try again.',
       });
     }
     if (results.length > 0) {
-      const user = results[0];
-      bcrypt.compare(password, user.Password, (err, isMatch) => {
+      const USER = results[0];
+      BCRYPT.compare(password, USER.Password, (err, isMatch) => {
         if (err) {
           //this only occurs when there is an error with bcrypt
           console.error('Error comparing passwords:', err);
-          return notifier.notify({
+          return NOTIFIER.notify({
             title: 'Error',
             message: 'Error with password checker. Try again.',
           });
         };
         if (isMatch) {
-          console.log('Login successful:', user);
+          console.log('Login successful:', USER);
+          loggingUserIntoJSON('xVennce');
           return res.redirect('/game');
         };
         //this occurs when there is a matching username but incorrect password
-        return notifier.notify({
+        return NOTIFIER.notify({
           title: 'Error',
           message: 'Incorrect password. Try again.',
         });
@@ -122,7 +123,7 @@ function loginUser(username, password, res) {
     else {
       //this else statement occurs when there is no matching username 
       //with the given username
-      notifier.notify({
+      NOTIFIER.notify({
         title: 'Error',
         message: 'Username not found. Try again.'
       });
@@ -130,16 +131,42 @@ function loginUser(username, password, res) {
   });
 };
 
+//Theses set of functions will be used for session control
+//It will log the current user into the JSON file
+//If there requires an update to score it shall do update it
+//On logout it will remove the user from the JSON file
+//
 //these functions need to be async as to allow for the code to be temp paused
 //whilst it writes to the database
 
 //This function will input the user information into the .json file to be held as a form of session control
+//There could be an oversight with how it only uses username rather than
+//UserID, however this shouldn't be an issue due to not duplicate usernames
 async function loggingUserIntoJSON(username){
-  const [rows] = await con.execute('SELECT * FROM userinfo WHERE username = ?', [username]);
-  if (rows.length === 0) {
-    console.log("No user found.");
+  const SECUREFOLDER = PATH.resolve(__dirname, 'secure');
+  const FILEPATH = PATH.join(SECUREFOLDER, '/json/userdata.json');
+  try {
+    //check to see if the folder exists
+    try{
+      await FS.access(SECUREFOLDER);
+    } catch (err) {
+      console.error(`Error accessing secure folder (${SECUREFOLDER}): ${err.message}`);
+    };
+    const QUERY = 'SELECT * FROM userinfo WHERE username = ?';
+    con.query(QUERY, [username], async (err, results) => {
+      if (err){
+        console.error('Error querying database:', err.message);
+      };
+      //this occurs when there is a matching username
+      if (results.length > 0) {
+        const USER = results[0];
+        console.log('User information logged into JSON file.');
+        await FS.writeFile(FILEPATH, JSON.stringify(USER, null, 2));
+      }
+    });
+  } catch (err) {
+    console.error('Error whilst writing to JSON file.', err.message);
   };
-  await fs.writeFile(out)
 };
 
 async function updatingUserInJSON(score){
@@ -149,16 +176,16 @@ async function updatingUserInJSON(score){
 //This function will clear the .json file of all user information
 async function clearingUserDataInJSON(){
   try{
-    const secureFolder = path.resolve(__dirname, 'secure');
-    const filePath = path.join(secureFolder, '/json/userdata.json');
+    const SECUREFOLDER = PATH.resolve(__dirname, 'secure');
+    const FILEPATH = PATH.join(SECUREFOLDER, '/json/userdata.json');
     //this try block is used to check if folder exists
     try{
-      await fs.access(secureFolder);
+      await FS.access(SECUREFOLDER);
     } catch (err) {
-      console.error(`Error accessing secure folder (${secureFolder}): ${err.message}`);
+      console.error(`Error accessing secure folder (${SECUREFOLDER}): ${err.message}`);
       return;
     };
-    await fs.writeFile(filePath, '');
+    await FS.writeFile(FILEPATH, '');
     console.log('Cleared userdata.json successfully.');
   } catch{
     console.error('Error whilst clearing JSON.');
@@ -167,46 +194,46 @@ async function clearingUserDataInJSON(){
 
 //this is the route for the main page
 //this is the page that loads when you go to the website
-app.get('/index', (req, res, next) => {
+APP.get('/index', (req, res, next) => {
   res.sendFile(__dirname + '/static/HomePage.html')
   clearingUserDataInJSON();
 });
 
 //this is the route for the login page
-app.get('/loginPage', (req, res, next) => {
+APP.get('/loginPage', (req, res, next) => {
   res.sendFile(__dirname + '/static/LoginPage.html')
 });
 
 //this is the route for the game page
-app.get('/game', (req, res, next) => {
+APP.get('/game', (req, res, next) => {
   res.sendFile(__dirname + '/static/GameFile.html')
 });
 //this is the route for the 404 page link, not 404 error
-app.get('/404', (req, res, next) => {
+APP.get('/404', (req, res, next) => {
   res.sendFile(__dirname + '/static/404.html')
 });
 
 //this listens for this post to inact the register user function
-app.post('/register', (req, res) => {
+APP.post('/register', (req, res) => {
   const { username, password } = req.body;
   registerUser(username, password, res);
 });
 
 //this listens for this post to inact the login user function
-app.post('/login', (req, res) => {
+APP.post('/login', (req, res) => {
   const { username, password } = req.body;
   loginUser(username, password, res);
 });
 
 //this is the route for the 404 page
 //this page should only appear when there is not a route for the url
-app.use(function(req, res){
+APP.use(function(req, res){
   //__dirname
   console.log(__dirname);
   res.status(404).sendFile(__dirname + '/static/404.html')
 })
   
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+APP.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 });
 
